cffc5103c0afe49734aaffcd1d70ab243dfcd2eb
infsysapp <ramcpuinfsys@gmail.com>
2024-12-18 10:38:00 -0400

    Initial commit
    The ideas and basis of the project.

---
+++ js/field_abstract.js
@@ -0,0 +1,139 @@
+var forest_01 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 30, // Percent per round. If random_x >= CreatureAppearanceChance then summon creature by creature type.
+    'CreatureTypes' : [ 'goblin', 'thieve' ], // Only one type per round.
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 2 // Might be more than one creature but of same type.
+};
+
+var forest_02 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 40,
+    'CreatureTypes' : [ 'goblin', 'thieve' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 2
+};
+
+var forest_03 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 50,
+    'CreatureTypes' : [ 'goblin' ],
+    'MaximumCreaturesPerRound' : 3,
+    'MaximumCreatureRounds' : 1
+};
+
+var forest_04 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'spider', 'tiger', 'bear' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 2
+};
+
+var forest_05 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'orc' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 2
+};
+
+var swamp_01 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'orc' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 2
+};
+
+var swamp_01_boss = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 100,
+    'CreatureTypes' : [ 'orcGeneral' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 1
+};
+
+var mountain_01 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'thieve', 'lion' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 2
+};
+
+var mountain_02 = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'eagle', 'lion' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 2
+};
+
+var rainy_mountain = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'eagle', 'lion' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 2
+};
+
+var fiery_mountain = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'eagle', 'lion' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 1
+};
+
+var fiery_mountain_boss = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 100,
+    'CreatureTypes' : [ 'dragon' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 1
+};
+
+var forgotten_mine = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 60,
+    'CreatureTypes' : [ 'goblin', 'bat' ],
+    'MaximumCreaturesPerRound' : 1,
+    'MaximumCreatureRounds' : 1
+};
+
+var foggy_mountain = {
+    'ImagePath' : '',
+    'MusicPath' : '',
+    'BattleMusicPath' : '',
+    'CreatureAppearanceChance' : 0,
+    'CreatureTypes' : [ 'dragon' ],
+    'MaximumCreaturesPerRound' : 2,
+    'MaximumCreatureRounds' : 1
+};
---
+++ js/player_abstract.js
@@ -0,0 +1,101 @@
+var player_abstract = {
+    'Life' : 100,
+    'PlayerName' : '',
+    'PlayerImagePath' : ''
+  };
+
+  var sheet_1 = {
+    'Class' : 'Fighter',
+    'Level' : 1,
+    'Race' : 'Human',
+    'Background' : 'Folk hero',
+    'Alighnment' : 'Lawful good',
+
+    'Strength' : 16,
+    'Dexterity' : 19,
+    'Constitution' : 17,
+    'Intelligence' : 11,
+    'Wisdom' : 14,
+    'Charisma' : 8,
+
+    'ArmorClass' : 14,
+    'Hitpoint' : { 'Current' : 0, 'Maximum' : 12 },
+    'HitDice' : { 'Hit' : 0, 'Total' : '1d10' },
+    'DeathSaves' : { 'Successes' : '0/3', 'Failures' : '0/3' },
+
+    'Attacks' : { 'Greatsword' : { 'Bonus' : 6, 'Damage' : '2d6' }, 'Longbow' : { 'Bonus' : 10, 'Damage' : '1d8' } },
+
+    'Equipment' : { 'Arrows': 20, 'Rations' : 10, 'Coins' : 10 }
+  }
+
+  var sheet_2 = {
+    'Class' : 'Wizard',
+    'Level' : 1,
+    'Race' : 'Human',
+    'Background' : 'Acolyte',
+    'Alighnment' : 'Chaotic good',
+
+    'Strength' : 10,
+    'Dexterity' : 17,
+    'Constitution' : 16,
+    'Intelligence' : 19,
+    'Wisdom' : 13,
+    'Charisma' : 7,
+
+    'ArmorClass' : 12,
+    'Hitpoint' : { 'Current' : 0, 'Maximum' : 8 },
+    'HitDice' : { 'Hit' : 0, 'Total' : '1d6' },
+    'DeathSaves' : { 'Successes' : '0/3', 'Failures' : '0/3' },
+
+    'Attacks' : { 'Shortsword' : { 'Bonus' : 6, 'Damage' : '1d6' }, 'Spells' : { 'Bonus' : 10, 'Damage' : '2d8' } },
+
+    'Equipment' : { 'Scrolls': 10, 'Rations' : 10, 'Coins' : 10 }
+  }
+
+  var sheet_3 = {
+    'Class' : 'Fighter',
+    'Level' : 1,
+    'Race' : 'Human',
+    'Background' : 'Noble',
+    'Alighnment' : 'Lawful neutral',
+
+    'Strength' : 19,
+    'Dexterity' : 8,
+    'Constitution' : 17,
+    'Intelligence' : 11,
+    'Wisdom' : 14,
+    'Charisma' : 16,
+
+    'ArmorClass' : 17,
+    'Hitpoint' : { 'Current' : 0, 'Maximum' : 12 },
+    'HitDice' : { 'Hit' : 0, 'Total' : '1d10' },
+    'DeathSaves' : { 'Successes' : '0/3', 'Failures' : '0/3' },
+
+    'Attacks' : { 'Greataxe' : { 'Bonus' : 8, 'Damage' : '1d12' }, 'Javelin' : { 'Bonus' : 8, 'Damage' : '2d6' } },
+
+    'Equipment' : { 'Javelin': 5, 'Rations' : 10, 'Coins' : 30 }
+  }
+
+  var sheet_4 = {
+    'Class' : 'Cleric',
+    'Level' : 1,
+    'Race' : 'Human',
+    'Background' : 'Soldier',
+    'Alighnment' : 'Neutral good',
+
+    'Strength' : 16,
+    'Dexterity' : 7,
+    'Constitution' : 17,
+    'Intelligence' : 10,
+    'Wisdom' : 19,
+    'Charisma' : 13,
+
+    'ArmorClass' : 18,
+    'Hitpoint' : { 'Current' : 0, 'Maximum' : 11 },
+    'HitDice' : { 'Hit' : 0, 'Total' : '1d8' },
+    'DeathSaves' : { 'Successes' : '0/3', 'Failures' : '0/3' },
+
+    'Attacks' : { 'Warhammer' : { 'Bonus' : 6, 'Damage' : '1d8' }, 'Throwingknife' : { 'Bonus' : 6, 'Damage' : '2d12' } },
+
+    'Equipment' : { 'Throwingknife': 2, 'Rations' : 10, 'Coins' : 15 }
+  }
